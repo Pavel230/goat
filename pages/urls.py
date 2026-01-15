571770cfdbc249a01d76d8349b9c3fc7adaa1e7c
Pavel Asaev <asaevpavel007@gmail.com>
2026-01-15 23:45:50 +0500

    Add static pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("delivery/", views.delivery, name="delivery"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("catalog/", views.catalog, name="catalog"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html")
+
+def about(request):
+    return render(request, "pages/about.html")
+
+def delivery(request):
+    return render(request, "pages/delivery.html")
+
+def contacts(request):
+    return render(request, "pages/contacts.html")
+
+def catalog(request):
+    return render(request, "pages/catalog.html")
